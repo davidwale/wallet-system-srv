bcc5588f716984217c53c713184367c3df5aaa1d
David Oyewale <davidwale2003@gmail.com>
2025-04-10 15:51:43 +0100

    make password column nullable

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -10,7 +10,7 @@ config();
 const configService = new ConfigService();
 
 export default new DataSource({
-  type: 'postgres',
+  type: 'mysql',
   host: configService.get('DB_HOST'),
   port: configService.get('DB_PORT'),
   username: configService.get('DB_USER'),
